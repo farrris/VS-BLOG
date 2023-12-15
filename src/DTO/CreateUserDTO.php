2103d43a50d999b7ef8770fd9952cfa4fceddd49
farrrris <cyutht3t.30@gmail.com>
2023-12-15 23:03:09 +0300

    add dto validation

--- src/DTO/CreateUserDTO.php
+++ src/DTO/CreateUserDTO.php
@@ -2,9 +2,15 @@
 
 namespace App\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class CreateUserDTO {
     public function __construct(
+        #[Assert\NotBlank(message: "Заполните email")]
+        #[Assert\Email(message: "Dведенное значение не является email")]
         public readonly string $email,
+        #[Assert\NotBlank(message: "Заполните пароль")]
+        #[Assert\Length(min: 4, minMessage: "Пароль должен содержать минимум 4 символа")]
         public readonly string $password
     )
     {
